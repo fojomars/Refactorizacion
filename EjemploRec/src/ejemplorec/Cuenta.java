75f4bc13ec2d4b1cdc8cbe6a2d892dee7140d50b
fojomars <fojomars@victoria130.danielcastelao.org>
2018-10-25 09:18:20 +0200

    Rename y format

--- EjemploRec/src/ejemplorec/Cuenta.java
+++ EjemploRec/src/ejemplorec/Cuenta.java
@@ -9,63 +9,64 @@ package ejemplorec;
  *
  * @author fojomars
  */
-public class Conta {
-        //Variables
+public class Cuenta {
+    //Variables
+
     private String nome;
     private String numConta;
     private double saldo;
-    
+
     //Constructor por defecto
-    public Conta(){
+    public Cuenta() {
     }
-    
+
     //Constructor con parámetros
-    public Conta(String nome, String nConta, double saldo){
+    public Cuenta(String nome, String nConta, double saldo) {
         this.nome = nome;
         numConta = nConta;
         this.saldo = saldo;
     }
-    
+
     //Métodos set y get de nome
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    public String getNome(){
+
+    public String getNome() {
         return nome;
     }
-    
+
     //Métodos set y get de número de conta
-    public void setNumConta(String numConta){
+    public void setNumConta(String numConta) {
         this.numConta = numConta;
     }
-    
-    public String getNumConta(){
+
+    public String getNumConta() {
         return numConta;
     }
-    
+
     //Métodos set y get de saldo
-    public void setSaldo(double saldo){
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    
-    public double getSaldo(){
+
+    public double getSaldo() {
         return saldo;
     }
-    
+
     //Métodos de ingreso y reintegro
-    public double ingresar(double cantIngresar){
+    public double ingresar(double cantIngresar) {
         saldo += cantIngresar;
         return saldo;
     }
-    
-    public double reintrego(double cantReintegro){
+
+    public double reintrego(double cantReintegro) {
         saldo -= cantReintegro;
         return saldo;
     }
-    
-    public void amosar(){
-        System.out.println("Nombre cliente: " + nome + "\nNúmero de cuenta: "+ numConta + "\nSaldo: " + saldo
+
+    public void amosar() {
+        System.out.println("Nombre cliente: " + nome + "\nNúmero de cuenta: " + numConta + "\nSaldo: " + saldo
         );
     }
 }
